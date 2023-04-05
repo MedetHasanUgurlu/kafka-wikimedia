db9ab6c8dc7c506a663e976f3dc7c5299b7ad740
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-04-05 18:08:09 +0300

    V2

--- kafka-consumer-database/src/main/java/com/medron/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/medron/springboot/KafkaDatabaseConsumer.java
@@ -1,15 +1,24 @@
 package com.medron.springboot;
 
+import com.medron.springboot.entity.WikimediaData;
+import com.medron.springboot.repository.WikimediaDataRepository;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+    private final WikimediaDataRepository wikimediaDataRepository;
+    /*
     @KafkaListener(groupId = "myGroup",topics = "wikimedia-recent-change")
     public void Consume(String eventMessage){
         LOGGER.info(String.format("data received : %s",eventMessage));
-    }
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
+    }*/
 }
--- kafka-consumer-database/src/main/java/com/medron/springboot/entity/WikimediaData.java
+++ kafka-consumer-database/src/main/java/com/medron/springboot/entity/WikimediaData.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "wikimedia-data")
 public class WikimediaData {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
